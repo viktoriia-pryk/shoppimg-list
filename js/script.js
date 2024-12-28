47614976a6149a8b8cd848d200bb1c91c033d89d
viktoriia-pryk <viktoriia.pryk98@gmail.com>
2024-12-28 11:44:56 +0200

    Removing items from the list

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
 
-//Event Listeners
 function addItem(e) {
   e.preventDefault();
-
+  
   const newItem = itemInput.value;
-
+  
   if (newItem.value === "") {
     alert("Please add an item!");
     return;
   }
-
+  
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(newItem));
-
+  
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
   itemList.appendChild(li);
@@ -35,5 +35,23 @@ function createIcon(classes) {
   icon.classList = classes;
   return icon;
 }
+function removeItem(e) {
+  if(e.target.parentElement.classList.contains('remove-item')){
+    e.target.parentElement.parentElement.remove()
+    
 
+  }
+
+  
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild)
+    
+  }
+}
+//Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
